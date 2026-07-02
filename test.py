ccf74974217fca9f5ca05dfa919febf805925ecb
AdityaKumar3594 <camp25674@example.com>
2026-07-02 19:17:34 +0530

    implemented and tested summarizer

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from utils.audio_processor import chunk_audio, process_input
 from core.transcriber import transcribe_all
+from core.summarizer import summarize, generate_title
 
 source="https://youtu.be/0jiLmhGzjOM?si=dWVOa4tiCbYp36zO"
 
 chunks=process_input(source)
-print(transcribe_all(chunks, language="english"))
+print(summarize(transcribe_all(chunks, language="english")))
+print(generate_title(transcribe_all(chunks, language="english")))
